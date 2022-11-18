89bb2c8320b564abb7aefe8c3891a4e974fa91e3
matthew <matthew_komar@yahoo.com>
2022-11-18 14:41:50 -0500

    Update Tracker

---
+++ Team70_CavernPart/Assets/Scripts/SecondDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondDisplay : MonoBehaviour
+{
+    [SerializeField] int displayIndex;
+    
+    void Start()
+    {
+        if (Display.displays.Length > 1)
+        {
+            Display.displays[displayIndex].Activate(2560, 1440, 60);
+        }
+    }
+    
+}
